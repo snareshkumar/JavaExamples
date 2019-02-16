59b773c133ec41895aa993f1432f54e018568a07
snareshkumar <naresh.samesh@gmail.com>
2019-02-16 16:22:13 +0530

    Stream example added

--- Java8Streams/src/com/naresh/streamtest/StreamTest.java
+++ Java8Streams/src/com/naresh/streamtest/StreamTest.java
@@ -1,6 +1,7 @@
 package com.naresh.streamtest;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.naresh.util.GenarateUtil;
@@ -8,13 +9,24 @@ import com.naresh.util.GenarateUtil;
 public class StreamTest {
 
 	public static void main(String[] args) {
-		
+
 		List<Person> persons = GenarateUtil.generatePersonList();
 
-		// Stream the Person list
-		persons.stream().map(p->p.getAge()).filter(p -> p > 5).collect(Collectors.toList());
+		// Stream Test - Get Only Ages which is greater than 30 - Output list only has
+		// ages which is greater than 30
+		// Here Map is the stream function which is only getting age value and condition
+		// given in the stream filter
 
-		System.out.println("person values are:::" + persons.size());
+		List<Integer> result = persons.stream().map(p -> p.getAge()).filter(p -> p.intValue() > 30)
+				.collect(Collectors.toList());
+		System.out.println("Result Size Is :::" + result);
+
+		// Getting List of Names from HashMap using stream and filter
+
+		Map<String, Person> persons1 = GenarateUtil.generateMaps();
+		List<String> firstNames = persons1.entrySet().stream().map(p -> p.getValue().getPersonFirstName())
+				.filter(p -> p.equalsIgnoreCase("jahnavi")).collect(Collectors.toList());
+		System.out.println("firstNames:::" + firstNames);
 
 	}
 
--- Java8Streams/src/com/naresh/util/GenarateUtil.java
+++ Java8Streams/src/com/naresh/util/GenarateUtil.java
@@ -1,22 +1,36 @@
 package com.naresh.util;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Random;
+import java.util.Map;
 
 import com.naresh.streamtest.Person;
 
 public class GenarateUtil {
-
-	static Random random;
-	static List<Person> persons = new ArrayList<>();
-
+	/**
+	 * 
+	 * @return
+	 */
 	public static List<Person> generatePersonList() {
-		for (int i = 0; i < 10; i++) {
-			Person person = new Person(100+i, "Sam" + i, "Anderson" + i, "Richard" + i, i);
-			persons.add(person);
-		}
-		return persons;
+
+		return Arrays.asList(new Person(100, "Jahnavi", "Naresh", "Kumar", 2),
+				new Person(101, "Ramesh", "Jagan", "Samson", 20), new Person(102, "Ravindra", "Sir", "Jadeja", 27),
+				new Person(103, "Rishap", "Pant", "Sarwan", 25), new Person(103, "Rishap", "Pant", "Sarwan", 25),
+				new Person(105, "Mahendra", "Singh", "Dhone", 26), new Person(106, "Yuvraj", "Swapnam", "Sigh", 29),
+				new Person(107, "Manish", "Pandey", "Kumar", 34), new Person(108, "Rajat", "Patiya", "Narmar", 33),
+				new Person(109, "VVS", "Laxman", "Reddy", 66));
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public static Map<String, Person> generateMaps(){
+		Map<String, Person> personsMap = new HashMap<>();
+		personsMap.put("100", new Person(100, "Jahnavi", "Naresh", "Kumar", 2));
+		personsMap.put("101", new Person(101, "Ramesh", "Jagan", "Samson", 20));
+		personsMap.put("102", new Person(102, "Ravindra", "Sir", "Jadeja", 27));
+		return personsMap;
 	}
 
 }
