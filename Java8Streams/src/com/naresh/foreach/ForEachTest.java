f0a59e9c21bd60e2825421825a21dfec782e63dd
snareshkumar <naresh.samesh@gmail.com>
2019-05-01 19:23:47 +0530

    Added Java 8 ForEach Example

---
+++ Java8Streams/src/com/naresh/foreach/ForEachTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.naresh.foreach;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * @author Naresh Kumar S
+ *
+ */
+public class ForEachTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,0);
+		List<Integer> copyNumbers = new ArrayList<>() ;
+		
+		// Before Java 8
+		
+		Iterator itr = numbers.iterator();
+		while(itr.hasNext()) {
+			System.out.println("Number values is:::"+itr.next());
+		}
+		
+		// Java 8 For Each
+		
+		numbers.forEach(new Consumer<Integer>() {
+
+			@Override
+			public void accept(Integer arg0) {
+				System.out.println("Values are :::"+arg0);
+				copyNumbers.add(arg0);
+			}
+			
+		});
+		
+		MyConsumer consumer = new MyConsumer();
+		copyNumbers.forEach(consumer);
+
+	}
+	
+	
+	
+
+}
+
+class MyConsumer implements Consumer<Integer>
+{
+
+	@Override
+	public void accept(Integer t) {
+		System.out.println("Iterated Values are:::"+t);
+		
+	}
+	
+}
