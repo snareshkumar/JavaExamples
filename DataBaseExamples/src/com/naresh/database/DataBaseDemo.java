1fcb90964c13ffc7e155f520b4db50641e75eb76
snareshkumar <naresh.samesh@gmail.com>
2019-05-06 17:10:29 +0530

    changes

--- DataBaseExamples/src/com/naresh/database/DataBaseDemo.java
+++ DataBaseExamples/src/com/naresh/database/DataBaseDemo.java
@@ -10,11 +10,12 @@ public class DataBaseDemo {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.10.124:3306/user", "vrmuser",
-				"vrmuser");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/user", "root", "");
+		Connection connection1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/cdvr", "root", "");
 		String sqlQuery = "insert into user.user(accountId,disabled,maxsessions,occupied,occupiednumrecordings,occupiedBytes,quota,scheduled,role,username)values(?,?,?,?,?,?,?,?,?,?)";
 		PreparedStatement ps = connection.prepareStatement(sqlQuery);
-		for(int i=0;i<=100000;i++) {
+		System.out.println("====Transaction starts======");
+		for (int i = 0; i <= 100000; i++) {
 			ps.setString(1, "defaultaccount");
 			ps.setInt(2, 0);
 			ps.setInt(3, -1);
@@ -24,11 +25,30 @@ public class DataBaseDemo {
 			ps.setInt(7, 0);
 			ps.setInt(8, 0);
 			ps.setString(9, "user");
-			ps.setString(10, "userId"+i);
-			System.out.println("=====i===="+i);
+			ps.setString(10, "userId" + i);
 			ps.execute();
 		}
-		
+		System.out.println("=====User Insertion is Succssful======");
+
+		String recordSqlQuery = "insert into cdvr.records(programId,scheduleId,userId,name,startTime,duration,status,protected,channelId,channelName)values(?,?,?,?,?,?,?,?,?,?)";
+		PreparedStatement ps1 = connection1.prepareStatement(recordSqlQuery);
+		for (int i = 0; i < 100000; i++) {
+			for (int j = 0; j <= 1; j++) {
+				ps1.setString(1, "programid" + i);
+				ps1.setString(2, "scheduleid" + i);
+				ps1.setString(3, "userId" + i);
+				ps1.setString(4, "testname");
+				ps1.setLong(5, Long.parseLong("155139840000" + j));
+				ps1.setInt(6, 60);
+				ps1.setString(7, "Completed");
+				ps1.setInt(8, 0);
+				ps1.setString(9, "123Channelname");
+				ps1.setString(10, "ChannelName123");
+				ps1.execute();
+
+			}
+		}
+
 		System.out.println("Completed the transactions");
 
 	}
